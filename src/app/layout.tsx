ead71f21c4fe91285accbabbc5d681253a130053
jonathanferreirar <jonathanferreirar@gmail.com>
2024-01-13 13:57:14 -0300

    :todo: start config project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Dev Store',
-  description: 'This is Store app, created by Jonathan Rodrigo, his popurew just enhanced my skills as a developer',
+  description:
+    'This is Store app, created by Jonathan Rodrigo, his popurew just enhanced my skills as a developer',
 }
 
 export default function RootLayout({
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
   )
 }
