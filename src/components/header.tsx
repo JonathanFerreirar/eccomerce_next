0f55c44efbc1226b7b06d3ae0286906bcbb89f40
jonathanferreirar <jonathanferreirar@gmail.com>
2024-01-14 01:19:08 -0300

    :sparkles: feat: header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
 
 export function Header() {
   return (
@@ -22,6 +23,17 @@ export function Header() {
           <ShoppingBag className="h-4 w-4" />
           <span className="text-sm"> Cart (o)</span>
         </div>
+        <div className="w-px h-4 bg-zinc-700" />
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm"> Account</span>
+          <Image
+            src="https://github.com/jonathanferreirar.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt="Profile user photo"
+          />
+        </Link>
       </div>
     </div>
   )
