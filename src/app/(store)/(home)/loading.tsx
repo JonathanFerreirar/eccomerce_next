31a1ece75cbecfee10d83c2f4a19b9b865f3a471
jonathanferreirar <jonathanferreirar@gmail.com>
2024-01-15 02:13:43 -0300

    :sparkles: feat: finish home page

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,13 @@
+import Skeleton from '@/components/skeleton'
+
 const Loading = () => {
-  return <div>loading....</div>
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6  h-[820px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
+  )
 }
 
 export default Loading
